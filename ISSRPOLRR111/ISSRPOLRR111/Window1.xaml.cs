d03c3555c7ae42998968a4754279fddd953a2ff7
Ravil <ravilgaliullin05@mail.ru>
2023-03-13 12:07:42 +0300

    Добавлены обработчики событий

--- ISSRPOLRR111/ISSRPOLRR111/Window1.xaml.cs
+++ ISSRPOLRR111/ISSRPOLRR111/Window1.xaml.cs
@@ -23,5 +23,10 @@ namespace ISSRPOLRR111
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
